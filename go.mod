d235c3f6507023da78ea86b41f7ca2d35af2e7d7
Anthony Ngo <anthonyngo002@gmail.com>
2021-04-05 15:44:38 -0700

    lexer works somewhat

---
+++ go.mod
@@ -0,0 +1,3 @@
+module bo
+
+go 1.16
--- tests/lexer_test.go
+++ tests/lexer_test.go
@@ -3,6 +3,7 @@ package lexer
 import (
 	"testing"
 
+	"bo/lexer"
 	"bo/token"
 )
 
@@ -24,7 +25,7 @@ func TestNextToken(t *testing.T) {
 		{token.EOF, ""},
 	}
 
-	l := New(input)
+	l := lexer.New(input)
 
 	for i, tt := range tests {
 		tok := l.NextToken()
